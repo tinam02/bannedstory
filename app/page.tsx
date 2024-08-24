432c9653f8322d257425b0c3b952ddb15291d6c9
Tina <oge612@gmail.com>
2024-08-24 10:14:16 +0200

    font

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Char from '@/components/atoms/Char';
 import Platform from '@/components/atoms/Platform';
 import Closet from '@/components/molecules/Items/Closet/Closet';
+import { fontArial } from './styles/fonts';
 
 export default async function Home({ searchParams }: { searchParams: any }) {
   return (
-    <main>
+    <main className={fontArial}>
       <Char reqBody={{}} />
       <Platform />
       <Closet />
--- app/styles/fonts.ts
+++ app/styles/fonts.ts
@@ -1,12 +1,15 @@
 import localFont from 'next/font/local';
 
-const fontRegupix = localFont({
+const fontRegupixInit = localFont({
   src: '../../public/font/Regupix.woff2',
   variable: '--font-regupix',
 });
-const fontArial = localFont({
+const fontArialInit = localFont({
   src: '../../public/font/ArialMT.woff2',
   variable: '--font-arial',
 });
+
+const fontRegupix = fontRegupixInit.className;
+const fontArial = fontArialInit.className;
 
 export { fontArial, fontRegupix };
