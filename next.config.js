801a9d54da8232d83232fb5a3c8afff35ab51d01
Tina <oge612@gmail.com>
2026-08-05 17:41:12 +0200

    clean

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // for verification builds that dont knock down dev local server
+  distDir: process.env.NEXT_DIST_DIR || '.next',
   images: {
     remotePatterns: [
       {
